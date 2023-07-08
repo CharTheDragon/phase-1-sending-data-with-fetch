30519f1bbd2ecd03c32355352af168530fa737ca
Nathaniel Swan <cygnet1.999@gmail.com>
2023-07-08 15:39:47 -0500

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    })
+        .then(function (resp) {
+            return resp.json();
+        })
+        .then(function (obj) {
+            document.body.innerHTML = obj['id'];
+        })
+        .catch(function (error) {
+            alert("I can't do it capn', I don't have the power!");
+            document.body.innerHTML = error.message;
+        });
+    
+}
